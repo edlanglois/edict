42a6aa5161965a1951bfcd073adf72739e36cfa6
Eric Langlois <eric@langlois.xyz>
2020-06-12 02:49:15 -0400

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
-"""Package setup script."""
-from typing import Optional
+#!/usr/bin/env python
 
 import setuptools
 
-try:
-    with open("README.md", "r") as f:
-        long_description: Optional[str] = f.read()
-except FileNotFoundError:
-    long_description = None
-
-setuptools.setup(
-    name="edict",
-    version="0.1.0",
-    author="Eric Langlois",
-    author_email="eric@langlois.xyz",
-    description="Eric's Dictionary Transformer",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=["edict"],
-    package_data={"edict": ["edict.lark"]},
-    entry_points={"console_scripts": ["edict=edict.cli:main"]},
-    install_requires=["lark-parser", "setuptools"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        # "License :: OSI Approved :: MIT License",
-    ],
-    python_requires=">=3.7",
-)
+if __name__ == "__main__":
+    setuptools.setup()
